5352335bd4af4ca60a656fad4d4f3c2a40b9af0d
Juan Marcelo Rodriguez <juanmrodriguez@alumnos.exa.unicen.edu.ar>
2026-05-19 12:24:38 -0300

    Update main menu categories

--- main_menu.sh
+++ main_menu.sh
@@ -6,11 +6,15 @@ BASE_DIR="/home/jmro/scripts"
 # Función para mostrar el menú principal
 show_main_menu() {
     echo "Seleccione una categoría:"
-    echo "1) System"
-    echo "2) Network"
-    echo "3) Backup"
-    echo "4) Development"
-    echo "5) Utilities"
+    echo "1) System (Mantenimiento, aceleración)"
+    echo "2) Network (IPs, Puertos, MAC)"
+    echo "3) Backup (Configuraciones, Paquetes)"
+    echo "4) Development (React, PHP, Servers)"
+    echo "5) Utilities (Duplicados, Passwords, EncFS)"
+    echo "6) Instaladores (LAMP Modular, VSCode, Brew)"
+    echo "7) Git (Clonación masiva)"
+    echo "8) Servicios (Gestor Zenity)"
+    echo "9) Juegos"
     echo "0) Salir"
     read -p "Ingrese una opción: " category
 }
@@ -50,6 +54,10 @@ while true; do
         3) list_and_execute_scripts "$BASE_DIR/backup" ;;
         4) list_and_execute_scripts "$BASE_DIR/dev" ;;
         5) list_and_execute_scripts "$BASE_DIR/utilities" ;;
+        6) list_and_execute_scripts "$BASE_DIR/instaladores" ;;
+        7) list_and_execute_scripts "$BASE_DIR/git" ;;
+        8) list_and_execute_scripts "$BASE_DIR/servicios" ;;
+        9) list_and_execute_scripts "$BASE_DIR/games" ;;
         0) echo "Saliendo..."; exit 0 ;;
         *) echo "Opción no válida." ;;
     esac
